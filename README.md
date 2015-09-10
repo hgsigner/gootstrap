fefd90fe733a45b30071e3f3e62e2f4fb94308e5
Hugo Miranda Mac <hugo@aioninnovations.com>
2015-09-10 00:33:35 -0700

    improve readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 #Gootstrap is a simple Go package bootstrapper.
+
+Gootstrap is a simple package that bootstrap new Go packages. It creates a REAME.md, a doc.go and main (package name).go file as a placeholder. I did it because I was repeating myself a lot when starting new projects.
+
+##Installing
+
+```bash
+$ go get github.com/hgsigner/gootstrap
+```
+
+##Usage
+After installing, you can use **gootstrap new package_name** to create a new project.
+
+```bash
+$ $GOPATH/bin/goostrap new project_name
+2015/09/10 00:06:36 Creating package project_name
+2015/09/10 00:06:36 Creating directory
+2015/09/10 00:06:36 Creating README.md
+2015/09/10 00:06:36 Creating main .go file.
+2015/09/10 00:06:36 Creating doc.go
+2015/09/10 00:06:36 Package created! cd project_name to access.
+$ cd project_name
+```
+###Tree
+```
+|-- project_name
+    |-- REAMDE.md
+    |-- project_name.go
+    |-- doc.go
+```
+
+I know that there is tons of things I could do to make it better, but, for now, it meets my needs. I be improving it.
+- - -
+##Licensing
+You can use or modify it to meet your needs.
--- gootstrap.go
+++ gootstrap.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 )
 
 var readmeTempl = `#%s
+
 This is the awesome description for %s.`
 
 var docTempl = `// Add some documentation to your package.
 package %s`
 
 var mainTempl = `package main
+
 import (
 	"fmt"
 )
@@ -24,16 +27,25 @@ func main() {
 
 func main() {
 
-	pack_name := os.Args[1]
-	fmt.Println(pack_name)
+	pack_name := os.Args[2]
+	log.Printf("Creating package %s", pack_name)
 
+	switch os.Args[1] {
+	case "new":
+		createPackage(pack_name)
+		log.Printf("Package created! cd %s to access.", pack_name)
+	}
+
+}
+
+func createPackage(pack_name string) {
 	sep := string(filepath.Separator)
 
 	// Creates the project's folder
-	err := os.Mkdir(pack_name, 0777)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+
+	if _, err := os.Stat(pack_name); os.IsNotExist(err) {
+		os.Mkdir(pack_name, 0777)
+		log.Println("Creating directory")
 	}
 
 	//Creates README.md
@@ -46,6 +58,7 @@ func main() {
 	defer readme_file.Close()
 	fReadme := fmt.Sprintf(readmeTempl, pack_name, pack_name)
 	readme_file.WriteString(fReadme)
+	log.Println("Creating README.md")
 
 	// Creates main .go file
 	mainpack := fmt.Sprintf("%s%s%s.go", pack_name, sep, pack_name)
@@ -56,6 +69,7 @@ func main() {
 	}
 	defer mainpack_file.Close()
 	mainpack_file.WriteString(mainTempl)
+	log.Println("Creating main .go file.")
 
 	// Creates main doc.go file
 	doc := fmt.Sprintf("%s%sdoc.go", pack_name, sep)
@@ -67,5 +81,5 @@ func main() {
 	defer doc_file.Close()
 	dReadme := fmt.Sprintf(docTempl, pack_name)
 	doc_file.WriteString(dReadme)
-
+	log.Println("Creating doc.go")
 }
